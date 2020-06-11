Bugfix for subject atlas generation. Adding fs111cereb average

databaseprep/nemo_merge_subject_parc_to_sparsemat.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 from scipy import sparse
+import scipy.stats
 import tempfile
 from pathlib import Path 
 import subprocess
@@ -36,6 +37,8 @@ refimg=nib.load(reffile)
 
 #parcname=fs86, fs86_sgmfix, fs111cereb, fs111cereb_sgmfix
 
+#each parcname and dilation takes 5-15min (shorter=no dilation)
+#for p in fs86 fs111cereb fs86_sgmfix fs111cereb_sgmfix; do outd=subjparc; mkdir -p $outd; tmpd=parctemp_${p}; mkdir -p $tmpd; for d in 0 1 2 3; do python nemo_merge_subject_parc_to_sparsemat.py nemo2/nemo_chunklist.npz ${p} nemo2/MNI152_T1_1mm_brain.nii.gz $outd/${p}_dil${d}_allsubj $d $tmpd; aws s3 sync $outd/ s3://kuceyeski-wcm-web-upload/nemo_atlases/ --exclude "*.pkl"; done; done; klaws stop
 
 s3paths=["s3://kuceyeski-wcm-temp/kwj2001/mrtrix_tckgen_%s/%s_MNI.nii.gz" % (s,parcname) for s in subjects]
 
@@ -54,13 +57,33 @@ def get_flattened_1mm(s3name):
     s3bucket=s3name.replace("s3://","").split("/")[0]
     s3key="/".join(s3name.replace("s3://","").split("/")[1:])
     filename=tmpdir+"/"+s3key.replace("/","_")
-    s3_client.download_file(s3bucket,s3key,filename)
+    if not Path(filename).is_file():
+        s3_client.download_file(s3bucket,s3key,filename)
     filename1mm=filename.replace(".nii.gz","_1mm.nii.gz")
-    subprocess.Popen([fsldir+"/bin/applywarp","-i",filename,"-r",reffile,"-o",filename1mm],env=dict(os.environ, FSLOUTPUTTYPE="NIFTI_GZ")).wait()
+    if not Path(filename1mm).is_file():
+        subprocess.Popen([fsldir+"/bin/applywarp","-i",filename,"-r",reffile,"-o",filename1mm,'--interp=nn'],env=dict(os.environ, FSLOUTPUTTYPE="NIFTI_GZ")).wait()
     #dilate the ROIs twice just to make sure they catch all (most?) of the streamline endpoints
     if numdil > 0:
-        subprocess.Popen([fsldir+"/bin/fslmaths",filename1mm]+dilarg+[filename1mm],env=dict(os.environ, FSLOUTPUTTYPE="NIFTI_GZ")).wait()
-    newvals=sparse.csr_matrix(nib.load(filename1mm).get_fdata().flatten(),dtype=np.uint16)
+        if(numdil==1):
+            dilprevfile=filename1mm
+        else:
+            dilprevfile=filename1mm.replace("_1mm.nii.gz","_1mm_dil%d.nii.gz" % (numdil-1))
+        dilfile=filename1mm.replace("_1mm.nii.gz","_1mm_dil%d.nii.gz" % (numdil))
+        
+        if Path(dilprevfile).is_file():
+            #if previous dilation file is found, just dilate that one time
+            subprocess.Popen([fsldir+"/bin/fslmaths",dilprevfile,'-dilD',dilfile],env=dict(os.environ, FSLOUTPUTTYPE="NIFTI_GZ")).wait()
+        else:
+            #perform all dilations
+            subprocess.Popen([fsldir+"/bin/fslmaths",filename1mm]+dilarg+[dilfile],env=dict(os.environ, FSLOUTPUTTYPE="NIFTI_GZ")).wait()
+    else:
+        dilfile=filename1mm
+    
+        #subprocess.Popen([fsldir+"/bin/fslmaths",filename1mm]+dilarg+[filename1mm],env=dict(os.environ, FSLOUTPUTTYPE="NIFTI_GZ")).wait()
+    #newvals=sparse.csr_matrix(nib.load(filename1mm).get_fdata().flatten(),dtype=np.uint16)
+    
+    newvals=sparse.csr_matrix(nib.load(dilfile).get_fdata().flatten(),dtype=np.uint16)
+    
     #newvals=sparse.csr_matrix(nib.processing.resample_from_to(nib.load(filename), refimg, order=0).get_fdata().flatten(),dtype=np.uint16)
     return newvals
 
@@ -81,6 +104,12 @@ P.close()
 print('took %.3f seconds' % (time.time()-starttime))
 
 sparse.save_npz(outputbase+".npz",Psparse_allsubj,compressed=False)
+
+mode,counts=scipy.stats.mode(Psparse_allsubj.toarray(),axis=0)
+imgmode=nib.Nifti1Image(np.reshape(mode,refimg.shape),affine=refimg.affine, header=refimg.header)
+nib.save(imgmode,outputbase+"_mode.nii.gz")
+
+print('Creating mode of %s took %.3f seconds' % (parcname,time.time()-starttime))
 
 
 def get_sparse_transform(isubj):